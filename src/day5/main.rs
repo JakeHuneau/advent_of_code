664987eaf85d86f5529216b4cfd3eb4f26f65b04
JakeHuneau <jakehuneau@yahoo.com>
2020-12-05 01:49:42 -0500

    Day 5

--- src/day5/main.rs
+++ src/day5/main.rs
@@ -1,25 +1,43 @@
-use advent_of_code::parse_file_delimiter;
+use advent_of_code::parse_file;
 use rayon::iter::ParallelIterator;
 use rayon::prelude::*;
-use std::collections::HashSet;
-use std::str::FromStr;
 
-// Check if each passport is valid
+// Convert the seats to binary -> u8 and then get seat ID from row * 8 + col. Find the max
 // Run in parallel since we can
 pub fn solver() {
-    let data = parse_file_delimiter::<String>("src/day5/input", "\r\n");
-    let max_seat = data.par_iter().map(|d| {
-        let (row, col) = translate(d);
-        row * 8 + col
-    }).max();
+    let data = parse_file::<String>("src/day5/input");
+    let max_seat = data
+        .par_iter()
+        .map(|d| {
+            let (row, col) = translate(d);
+            u16::from(row) * 8 + u16::from(col)
+        })
+        .max()
+        .unwrap();
     println!("max seat number: {:?}", max_seat);
 }
 
-// Same as above but with different check
+// Find sum of all seat IDs, then subtract that from the total sum of min_seat -> max_seat to find
+// the missing seat
 pub fn solver_extra() {
-    let data = parse_file_delimiter::<String>("src/day5/input", "\r\n");
+    let data = parse_file::<String>("src/day5/input");
+    let seat_sum = data
+        .par_iter()
+        .fold(
+            || 0_usize,
+            |sum, d| {
+                let (row, col) = translate(d);
+                sum + usize::from(row) * 8 + usize::from(col)
+            },
+        )
+        .sum::<usize>();
+    // We know the min seat is 38 and max is 998. So the expected sum total is
+    // (max - min + 1)(max + min) / 2
+    let expected_sum = (998 - 38 + 1) * (998 + 38) / 2;
+    println!("missing seat: {:?}", expected_sum - seat_sum);
 }
 
+// Switch those F with 0, B with 1, L with 0, and R with 1 and convert those binary to u8
 fn translate(seat: &String) -> (u8, u8) {
     let row = u8::from_str_radix(&seat[..7].replace("F", "0").replace("B", "1"), 2).unwrap();
     let column = u8::from_str_radix(&seat[7..].replace("L", "0").replace("R", "1"), 2).unwrap();
